db65396d2b6ceafa2aace8725585ed8afe8802bf
Abdelrahim Adam <hemaadam212@gmail.com>
2025-11-10 20:22:44 +0200

    Your message here

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     },
   },
 
   server: {
     port: 3000,
-    // 🔹 You can still use this for local backend testing (optional)
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
