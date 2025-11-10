632c645818fd52b52e2fdadba8bddc87ca5a1b8c
Abdelrahim Adam <133844991+AbdelrahimAdam@users.noreply.github.com>
2025-11-11 01:30:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,12 +8,6 @@ export default defineConfig({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
     'process.env': {},
     global: 'globalThis',
-  },
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
   },
 
   server: {
