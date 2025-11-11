69fd0bb6965e1d4b57f06c1af26571f098e708d0
Abdelrahim Adam <133844991+AbdelrahimAdam@users.noreply.github.com>
2025-11-11 03:14:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect, lazy } from 'react';
 import { Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from '@/hooks/useAuth';
+import { AuthProvider,useAuth } from '@/hooks/useAuth.tsx';
 import LoadingSpinner from '@/components/UI/LoadingSpinner.jsx';
 
 // Lazy load auth pages
